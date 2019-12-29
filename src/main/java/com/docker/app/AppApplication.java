aab61711d7d7a6743e0c8d357372025f858e9897
chenjia <jia.chen1@thoughtworks.com>
2019-12-29 23:35:28 +0800

    add method

--- src/main/java/com/docker/app/AppApplication.java
+++ src/main/java/com/docker/app/AppApplication.java
@@ -2,11 +2,19 @@ package com.docker.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
+    }
+
+    @GetMapping(value = "/")
+    public String say() {
+        return "hello docker";
     }
 }
